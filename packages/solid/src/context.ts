09d7423800f442be965e26377a28e3864ed6a2a7
Gabe Klein <gabe@termtm.com>
2026-02-19 14:57:33 -0500

    fix: update context.set call in solid Provider

--- packages/solid/src/context.ts
+++ packages/solid/src/context.ts
@@ -20,7 +20,7 @@ declare namespace Provider {
 function Provider<T extends State>(props: Provider.Props<T>) {
   const context = useContext(Lookup).push();
 
-  context.use(props.for, props.set && ((x) => void x.set(props.set!)));
+  context.set(props.for, props.set && ((x) => void x.set(props.set!)));
 
   onCleanup(() => context.pop());
 
