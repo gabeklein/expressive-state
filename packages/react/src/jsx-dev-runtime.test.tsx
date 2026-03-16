3eea7899e051da8288d10adf2f9f9652a9d46ab3
Gabe Klein <gabe@termtm.com>
2026-03-16 18:38:42 -0400

    test(jsx-dev-runtime): update State element conversion to use JSX syntax

--- packages/react/src/jsx-dev-runtime.test.tsx
+++ packages/react/src/jsx-dev-runtime.test.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource . */
+
 import { expect, it } from '../vitest';
 
 import { State } from '.';
@@ -14,7 +16,7 @@ it('exports Fragment', () => {
 it('will convert State to element', () => {
   class Test extends State {}
 
-  const element = jsxDEV(Test, { children: 'Hello' }, 'key', false);
+  const element = <Test>Hello</Test>;
 
   expect(element).toMatchObject({
     type: expect.any(Function),
